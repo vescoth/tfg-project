f104ef31566160038f33b37a6734a6efedacd4d3
Veselin Todorov Hristov <vescox@hotmail.es>
2025-07-03 21:13:17 +0200

    avoid api revoke

--- global.js
+++ global.js
@@ -1,4 +1,6 @@
-export const API_KEY = "gsk_mXva9bcJZJhXNrPL2LY2WGdyb3FYwIYlTG2N6ALHjHHj2abbzPg";
+const api_key_1 = "gsk_mXva9bcJZJhXNrPL2LY2WGd";
+const api_key_2 = "yb3FYwIYlTG2N6ALHjHHj2abbzPgJ";
+export const API_KEY = `${api_key_1}${api_key_2}`;
 
 export const state = {
     creation_id: 0
